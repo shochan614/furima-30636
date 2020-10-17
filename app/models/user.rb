a684332a0f4d5b9997f98fcc47b4330f4153b2d8
shochan614 <shochan.hina@gmail.com>
2020-10-17 16:29:55 +0900

    ルボコップ実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[A-z])(?=.*?[\d])[A-z\d]+\z/i.freeze #半角英数混合1字以上
-  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze #全角ひらカタ漢字
-  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze #全角カナ
+  PASSWORD_REGEX = /\A(?=.*?[A-z])(?=.*?[\d])[A-z\d]+\z/i.freeze # 半角英数混合1字以上
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze # 全角ひらカタ漢字
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze # 全角カナ
 
-  validates :password, length: { minimum: 6}, format: { with: PASSWORD_REGEX }
+  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
   validates :nickname, presence: true, uniqueness: true
   validates :first_name, presence: true, format: { with: ZENKAKU_REGEX }
   validates :last_name, presence: true, format: { with: ZENKAKU_REGEX }

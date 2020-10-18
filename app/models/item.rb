bed34a882c316900f09d6179321664ec1cbe829d
shochan614 <shochan.hina@gmail.com>
2020-10-18 13:44:26 +0900

    itemモデル　バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+    validates :category_id
+    validates :sales_status_id
+    validates :shopping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
   belongs_to :user
 end

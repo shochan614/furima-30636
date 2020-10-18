e901645a3c5d5ce91baa4cb06a16db6645f2184d
shochan614 <shochan.hina@gmail.com>
2020-10-19 08:11:38 +0900

    sales_statusのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
 
   with_options presence: true do
     validates :name

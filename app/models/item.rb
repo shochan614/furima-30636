d5b376ef72343c0a74ff0c60b022403c9df51cbf
shochan614 <shochan.hina@gmail.com>
2020-10-19 08:26:37 +0900

    shopping_fee_statusのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shopping_fee_status
 
   with_options presence: true do
     validates :name

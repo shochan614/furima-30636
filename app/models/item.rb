a5fee459b7bcad22581e87a3a1223e7a3924319b
shochan614 <shochan.hina@gmail.com>
2020-10-19 09:48:27 +0900

    scheduled_deliveryのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shopping_fee_status
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 
   with_options presence: true do
     validates :name

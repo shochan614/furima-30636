0ad2af8da551b7355e9f730b06152696042074e0
shochan614 <shochan.hina@gmail.com>
2020-10-19 09:26:15 +0900

    prefectureのバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,8 @@ class Item < ApplicationRecord
       validates :category_id 
       validates :sales_status_id
       validates :shopping_fee_status_id
+      validates :prefecture_id
     end
-    validates :prefecture_id
     validates :scheduled_delivery_id
   end
 

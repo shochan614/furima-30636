5f02035c0bc3364ef9c4a5fdfe6cce9f16914ff9
shochan614 <shochan.hina@gmail.com>
2020-10-21 14:41:30 +0900

    レビュー修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,13 +2,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     const addProfit = document.getElementById("profit")
     const tax = (inputValue * 0.1)
-    addTaxDom.innerHTML = tax;
+    addTaxDom.innerHTML = Math.floor(tax)
     const profit = (inputValue * 0.9)
-    addProfit.innerHTML = profit;
+    addProfit.innerHTML = Math.floor(profit);
 })
  
 });

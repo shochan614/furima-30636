e2f0f2cc5eb72be2ece182eb09b2df188c9bf20c
shochan614 <shochan.hina@gmail.com>
2020-10-21 21:29:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,7 +55,5 @@ end
 
 def move_to_index
   @item = Item.find(params[:id])
-  unless user_signed_in? && current_user.id == @item.user_id
-    redirect_to root_path
-  end
+  redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
 end

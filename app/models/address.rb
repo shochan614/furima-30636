82798f8f101e02ff6e02100e54cfb073ccacf575
shochan614 <shochan.hina@gmail.com>
2020-10-22 17:25:08 +0900

    addressテーブルのカラムに修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+
+  with_optinos precense: true do
+    validates :postal_code, format: { ^[0-9]{3}-[0-9]{4}$ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :house_number
+    validates :phone_number
+    validates 
+  end
 end

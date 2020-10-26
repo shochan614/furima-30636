528f13780aeedc72e856204329b9d1b982e18665
shochan614 <shochan.hina@gmail.com>
2020-10-27 01:19:55 +0900

    ルボコップ実行・コメント部分削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,9 @@ class OrdersController < ApplicationController
     @user_order = UserOrder.new(user_order_params)
     if @user_order.valid?
       @item = Item.find(params[:item_id])
-       pay_item
-       @user_order.save
-       redirect_to root_path
+      pay_item
+      @user_order.save
+      redirect_to root_path
     else
       @item = Item.find(params[:item_id])
       render :index
@@ -22,31 +22,27 @@ class OrdersController < ApplicationController
   end
 
   private
+
   def user_order_params
     params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: user_order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: user_order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
   end
 
   def reject
     @item = Item.find(params[:item_id])
-    if Purchase.exists?(item_id: @item.id)
-      redirect_to root_path
-    end
+    redirect_to root_path if Purchase.exists?(item_id: @item.id)
   end
-  
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -2,10 +2,9 @@ class UserOrder
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :building_name, :token
 
-
-  #addressのバリデーション
+  # addressのバリデーション
   with_options presence: true do
-    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/ }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :house_number

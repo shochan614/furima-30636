e0d789f3f36178a3bc60dab4207835c6748a43a8
shochan614 <shochan.hina@gmail.com>
2020-10-26 12:29:49 +0900

    modelの単体テストコードのexample記述

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     prefecture_id { Faker::Number.within(range: 2..48) }
     city { Faker::Address.city }
     house_number { Faker::Address.street_address }
+    building_name { Faker::Address.street_name }
     phone_number { Faker::Number.number(digits: 11) }
     user_id { Faker::Number.within(range: 1..100) }
     item_id { Faker::Number.within(range: 1..100) }
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe UserOrder, type: :model do
-  describe '何をテストするのか' do
+  describe '商品の購入' do
     before do
       @user_order = FactoryBot.build(:user_order)
     end
-    it "具体的なテスト項目" do 
+    it "全ての項目が正しく入力されていれば保存できること" do 
+    end
+    it "任意項目（建物名）が入力されていなくても保存できること" do 
+    end
+    it "postal_codeが空だと保存できないこと" do 
+    end
+    it "postal_codeにハイフンがないと保存できないこと" do 
+    end
+    it "postal_codeが全角数字では保存できないこと" do 
+    end
+    it "postal_codeが3桁-4桁以外は保存できないこと" do 
+    end
+    it "prefecture_idが1（未選択）では保存ができないこと" do 
+    end
+    it "cityが空だと保存できないこと" do 
+    end
+    it "house_numberが空だと保存ができないこと" do 
+    end
+    it "phone_numberが空だと保存ができないこと" do 
+    end
+    it "phone_numberが11桁以上だと保存ができないこと" do 
+    end
+    it "phone_numberが10桁未満だと保存ができないこと" do 
     end
   end
 end
